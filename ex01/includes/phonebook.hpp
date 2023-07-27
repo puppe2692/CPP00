1bd163f4f1c75fb05c0963950c0d8961606ffaa4
Nicolas WYSEUR <nwyseur@paul-f5Br4s4.clusters.42paris.fr>
2023-07-27 16:36:42 +0200

    CPP00_ex01 phonebook.cpp set up

--- ex01/includes/phonebook.hpp
+++ ex01/includes/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: nwyseur <nwyseur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 14:14:08 by nwyseur           #+#    #+#             */
-/*   Updated: 2023/07/27 15:32:08 by nwyseur          ###   ########.fr       */
+/*   Updated: 2023/07/27 16:36:16 by nwyseur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ class phonebook
 			void	ft_add_contact(std::string firstName, std::string lastName,
 						std::string nickName, std::string phoneNumber, std::string secret);
 			void	ft_show_contact(int index);
-			void	ft_show_list(int index);
+			void	ft_show_list(void);
 			int		ft_totalcontact(void);
 			int		ft_nbconctact(void);
 
 	private:
-			contact	carnet[8];
-			int		_totalContact;
-			int		_nbContact;
+			contact			carnet[8];
+			static int		_totalContact;
+			static int		_nbContact;
 };
 
 #endif
--- ex01/srcs/phonebook.cpp
+++ ex01/srcs/phonebook.cpp
@@ -6,7 +6,42 @@
 /*   By: nwyseur <nwyseur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 18:55:57 by nwyseur           #+#    #+#             */
-/*   Updated: 2023/07/26 18:55:59 by nwyseur          ###   ########.fr       */
+/*   Updated: 2023/07/27 16:29:57 by nwyseur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/phonebook.hpp"
+
+int	phonebook::_nbContact = 0;
+int	phonebook::_totalContact = 0; // pas bien compris les decla static et non static
+
+phonebook::phonebook(void)
+{
+	return;
+}
+
+phonebook::~phonebook(void)
+{
+	return;
+}
+
+void	phonebook::ft_add_contact(std::string firstName, std::string lastName,
+						std::string nickName, std::string phoneNumber, std::string secret)
+{
+	this->phonebook::carnet[phonebook::_nbContact].ft_set_firstname(firstName);
+	this->phonebook::carnet[phonebook::_nbContact].ft_set_lastname(lastName);
+	this->phonebook::carnet[phonebook::_nbContact].ft_set_nickname(nickName);
+	this->phonebook::carnet[phonebook::_nbContact].ft_set_phonenumber(phoneNumber);
+	this->phonebook::carnet[phonebook::_nbContact].ft_set_secret(secret);
+	phonebook::_nbContact++;
+	phonebook::_nbContact %= 8; // verifier pourquoi
+	if (phonebook::_totalContact < 8)
+	{
+		phonebook::_totalContact++;
+	}
+}
+
+void	phonebook::ft_show_contact(int index)
+{
+	
+}
